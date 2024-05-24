39e4f2a7d8efd465d3ee37a62318559f4a8982f3
Konstantin Kaznacheev <kazna.k.rad@yandex.ru>
2024-05-24 12:15:41 +0300

    Установил новые пакеты

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Responses;
+use App\Http\Requests\User\UserLoginRequest;
+use App\Http\Requests\User\UserRegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use Responses;
+
+    public function register(UserRegisterRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $data = (object)$request->validated();
+
+        $user = User::firstOrCreate([
+            'email' => $data->email,
+        ], [
+            'first_name' => $data->firstName,
+            'last_name' => $data->lastName,
+            'password' => Hash::make($data->password),
+        ]);
+
+        return $this->successResponseWithData(UserResource::make($user));
+    }
+
+    public function login(UserLoginRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $data = (object)$request->validated();
+
+        $user = User::whereEmail($data->email)->firstOrFail();
+        $token = $user->createToken('auth_token');
+
+        return $this->successResponseWithData([
+            'token' => $token->plainTextToken,
+        ]);
+    }
+}
