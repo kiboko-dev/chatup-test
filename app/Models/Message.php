cbdf84aaaada131c22714f16ec0a87aa46a564d8
Konstantin Kaznacheev <kazna.k.rad@yandex.ru>
2024-05-24 12:17:37 +0300

    Добавил сообщения

---
+++ app/Models/Message.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'messages';
+    protected $fillable = [
+        'chat_id',
+        'user_id',
+        'message'
+    ];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')
+    ->group(function () {
+        Route::prefix('auth')->group(function () {
+            Route::post('register', [AuthController::class, 'register']);
+            Route::post('login', [AuthController::class, 'login']);
+        });
+
+        Route::middleware('auth:sanctum')
+            ->group(function () {
+                Route::get('/users', [UserController::class, 'index']);
+                Route::get('/users/{user}', [UserController::class, 'show']);
+
+                Route::get('/chats', [ChatController::class, 'index']);
+            });
+    });
